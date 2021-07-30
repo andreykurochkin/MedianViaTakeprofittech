15cfdd855736821e18c34d6f22ec67db4d21cc64
andreykurochkin <andrey.kurochkin@hotmail.com>
2021-07-30 17:59:16 +0300

    done

--- MedianViaTakeprofittech.Tests/MedianViaTakeprofittech.UnitTests/MedianTests.cs
+++ MedianViaTakeprofittech.Tests/MedianViaTakeprofittech.UnitTests/MedianTests.cs
@@ -15,6 +15,7 @@ namespace TCPConnection.DomainTests {
         [InlineData(new int[] { 2, 2 }, 2.0)]
         [InlineData(new int[] { 16, 12, 99, 95, 18, 87, 10 }, 18.0)]
         [InlineData(new int[] { 9, 1, 0, 2, 3, 4, 6, 8, 7, 10, 5 }, 5)]
+        [InlineData(new int[] { 1,3,5,7}, 4)]
         public void ToDecimal_ShouldReturnExpectedValue_WhenDataIsValid(int[] array, decimal expected) {
             _sut = new(array);
 
